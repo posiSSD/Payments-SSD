dab51ff92f0605a4d7633964f1adb5b76296f496
Manuel Llaguno <manuel.llaguno@apuestatotal.com>
2022-02-14 12:16:08 -0500

    test visits

---
+++ kushki/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$con_host=env('DB_HOST');
+$con_db_name=env('DB_DATABASE');
+$con_user=env('DB_USERNAME');
+$con_pass=env('DB_PASSWORD');
+$mysqli = new mysqli($con_host,$con_user,$con_pass,$con_db_name,3306);
+
+if (mysqli_connect_errno()) {
+	printf("Conexion fallida: %s\n", mysqli_connect_error());
+	exit();
+}
+$mysqli->query("SET CHARACTER SET utf8");
+$date = date("Y-m-d H:i:s");
--- kushki/index.php
+++ kushki/index.php
@@ -1,1 +1,30 @@
+<?php 
+require '/var/www/payments.apuestatotal.app/kushki/env.php';
+require '/var/www/payments.apuestatotal.app/kushki/db.php';
+include '/var/www/html/sys/helpers.php';
+
+
+$log_dir = str_replace(strrchr($_SERVER['SCRIPT_FILENAME'], "/"), "", $_SERVER['SCRIPT_FILENAME'])."/log/";
+$log_file = date("Y-m-d").".log";
+log_init($log_dir,$log_file);
+
+$visit = [];
+$visit["init"]=date("Y-m-d H:i:s");
+$visit['ip']=$_SERVER['REMOTE_ADDR'];
+$visit['device']=$_SERVER['HTTP_USER_AGENT'];
+$visit['ref']=(array_key_exists('HTTP_REFERER', $_SERVER)?$_SERVER['HTTP_REFERER']:'direct');
+$visit['url']=$_SERVER['REQUEST_URI'];
+
+$visit_insert = "INSERT INTO 
+					tbl_visits 
+					(url,ip,server_info) 
+					VALUES ('".$visit["url"]."','".$visit['ip']."','".print_r($_SERVER,true)."')";
+$mysqli->query($visit_insert);
+if($mysqli->error){
+	echo $mysqli->error; die; 
+}
+$mysqli->close();
+log_write($visit);
+
+?>
 kushki
