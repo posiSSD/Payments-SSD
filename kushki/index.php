9dc2ef628d9f93c65c1969be99c59df4814a42da
Manuel Llaguno <manuel.llaguno@apuestatotal.com>
2022-02-14 13:15:30 -0500

    first websocket test

--- kushki/index.php
+++ kushki/index.php
@@ -3,6 +3,15 @@ require '/var/www/payments.apuestatotal.app/kushki/env.php';
 require '/var/www/payments.apuestatotal.app/kushki/db.php';
 include '/var/www/html/sys/helpers.php';
 
+// print_r($_SERVER); exit();
+$fv=time();
+
+$url = 'http';
+$url.= (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'');
+$url.= '://';
+$url.= (isset($_SERVER["HTTP_HOST"]) ? substr($_SERVER['HTTP_HOST'],0):"");
+$url.= "/kushki/";
+
 
 $log_dir = str_replace(strrchr($_SERVER['SCRIPT_FILENAME'], "/"), "", $_SERVER['SCRIPT_FILENAME'])."/log/";
 $log_file = date("Y-m-d").".log";
@@ -25,12 +34,49 @@ if($mysqli->error){
 }
 $mysqli->close();
 
+$auth_data = false;
+$auth_token = false;
+$user_id = false;
 if(isset($_GET['auth_data'])){
+	// [auth_token] => FAE2579BC8325A2F60B432173CEF4D77
+	// [user_id] => 3333200
+	// [avatarUrl] => https://static.springbuilder.site/assets/addon/avatar.png	
 	$auth_data = json_decode($_GET["auth_data"],true);
+	$auth_token = $auth_data["auth_token"];
+	$user_id = $auth_data["user_id"];
 	$visit["auth_data"]=$auth_data;
-	// print_r($auth_data);
+}
+
+if($auth_data){
+	if(in_array($user_id, [3333200])){
+		?>
+		<script type="text/javascript">
+			var this_url = "<?php echo "http".(array_key_exists("HTTPS", $_SERVER)?"s":"")."://".$_SERVER["HTTP_HOST"];?>";
+			// console.log(this_url);
+			var user_id=<?php echo $user_id;?>;
+			var auth_token="<?php echo $auth_token;?>";
+			var connecting = false;
+			var site_id = 279;
+			var sws;
+			// var ws_url = "wss://us-swarm-ws.betconstruct.com/";
+			var ws_url = "wss://eu-swarm-springre.betconstruct.com/";
+			// var ws_url = "wss://soe-swarm-apuesta.betconstruct.com/";
+			var message_queue = [];
+			var swsid;
+			var ws_session = false;
+			var fw_user = [];
+			var fw_active = true;
+			// var fw_active = false;
+			var interval_fw_get_balance = false;
+			var interval_sw_get_user_balance = false;
+		</script>
+		<script type="text/javascript" src="<?php echo $url;?>js/jquery-3.6.0.min.js?<?php echo $fv;?>"></script>
+		<script type="text/javascript" src="<?php echo $url;?>js/bc_ws.js?<?php echo $fv;?>"></script>
+		<script type="text/javascript" src="<?php echo $url;?>js/k.js?<?php echo $fv;?>"></script>
+
+		<?php
+	}
 }
 log_write($visit);
-
 ?>
-:)
+<div id="msg">:)</div>
---
+++ kushki/js/bc_ws.js
@@ -0,0 +1,146 @@
+function ws_has(){
+	// console.log("ws_has");
+	if(!("WebSocket" in window)){
+		return false;
+	}else{
+		return true;
+	}
+}
+function ws_connect(){
+	$(document).trigger("ws_connect", []);
+	console.log("ws_connect");
+	$("#msg").html('Conectando con BC...');
+	if(!connecting){
+		connecting = true;
+		if(ws_has()){
+			try{
+				sws = new WebSocket(ws_url);
+				sws.onopen = function(){
+					$("#msg").html('Conectado!');
+					console.log("Connected!");
+					sw_open_session();
+					connecting=false;
+				};
+				sws.onmessage = function(msg){
+					// console.log("onmessage");
+					var obj = jQuery.parseJSON(msg.data);
+					// console.log(obj);
+					// console.log(swsid);
+					if(obj.rid && obj.rid === "sw_open_session"){
+						$(document).trigger("sw_open_session", obj);
+						console.log("--------------> sw_open_session");
+						// console.log(obj);
+						swsid = obj.data.sid;
+						sw_restore_login();
+						// balance_history();
+					}else if(obj.rid === "restore_login"){
+						$(document).trigger("restore_login", obj);
+						// $("#msg").append('<br>Validando credenciales...');
+						console.log("--------------> restore_login");
+						console.log(obj.code);
+						if(obj.code == 12){
+							$(document).trigger("sw_login_error", obj);
+							// console.log("BAD LOGIN, BYE BYE");
+						}else{
+							// console.log("LOGIN OK");
+							// console.log(obj);
+							// console.log(user_id);
+							// console.log(auth_token);
+
+							// $("#msg").html('Obteniendo usuario...');
+							var next = false;
+							if(obj.data.auth_token === auth_token){
+								// console.log("auth_token es igual");
+								next=true;
+							}else{
+								// console.log("auth_token NO es igual");
+								next = false;
+							}
+							if(obj.data.user_id === user_id){
+								next=true;
+								// console.log("user_id es igual");
+							}else{
+								// console.log("user_id NO es igual");
+								next = false;
+							}
+							if(next){
+								console.log("restore_login OK");
+								$("#msg").html('Obteniendo usuario...');
+								sw_get_user_balance();
+							}else{
+								$(document).trigger("sw_login_error", obj);
+							}
+						}
+					}else if(obj.rid === "sw_get_user_balance"){
+						$(document).trigger("sw_get_user_balance", obj);
+						console.log("--------------> sw_get_user_balance");
+						// console.log(obj);
+						if(obj.code == 12){
+							console.log("BAD LOGIN sw_get_user_balance, BYE BYE");
+							// $(document).trigger("clearBetData");
+							// $(document).trigger("sw_login_error", obj);
+							// $(document).trigger("invalidSession", [data]);
+						}else{
+							console.log("LOGIN sw_get_user_balance OK");
+							console.log(obj);
+							$("#msg").html('Hola <b>'+obj.data.first_name+'</b>, tu saldo actual es S/'+obj.data.balance);
+							// $(document).trigger("sw_login_ok", obj);
+							// $(document).trigger("sw_get_user_balance_event", obj);
+							// return "sw_login_ok";
+							// $(document).trigger("updatesw_get_user_balance", [data.data]);
+						}
+					}
+					// console.log(swsid);
+				};
+				sws.onclose = function(){
+					$(document).trigger("ws_onclose", []);
+					// swsid ='';
+					connecting = false;
+					// console.log('Disconnected. Socket Status: '+sws.readyState+' (Closed)');
+				};
+			}
+			catch(exception){
+				$(document).trigger("ws_error", exception);
+				// console.log("Disconnected!");
+				connecting = false;
+			}
+		}else{
+			$(document).trigger("ws_nows", []);
+			console.log("NO WS, BYE");
+		}
+	}
+}
+function sw_restore_login(){
+	$("#msg").html('Validando credenciales...');
+	console.log("sw_restore_login");
+	if(typeof user_id != 'undefined' && user_id && typeof auth_token != 'undefined' && auth_token){
+		var _msg = '{"command": "restore_login","rid":"restore_login","params": {"user_id": '+user_id+',"auth_token": "'+auth_token+'"}}';
+		sw_queue_msg(_msg);
+	}
+}
+function sw_get_user_balance(){
+	if(!fw_active){ return false; }
+	$("#msg").html('Obteniendo usuario...');
+	console.log("sw_get_user_balance");
+	var _msg = '{"command":"get_user", "rid":"sw_get_user_balance"}';
+	sw_queue_msg(_msg);
+	return "sw_login_ok";
+}
+function sw_open_session(){
+	console.log("sw_open_session");
+	// $("#msg").html('Creando sesión...');
+	sw_send_msg('{"command":"request_session","rid":"sw_open_session","params":{"site_id": '+site_id+',"language":"spa"}}')
+}
+function sw_send_msg(msg){
+	if(sws && sws.readyState == 1){
+		sws.send(msg);
+	}
+}
+function sw_queue_msg(msg){
+	if(sws && sws.readyState == 1 && swsid){
+		sw_send_msg(msg);
+	}else{
+		message_queue.push(msg);
+		ws_connect();
+	}
+}
