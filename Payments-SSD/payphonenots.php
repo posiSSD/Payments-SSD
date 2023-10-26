16ae8abd054bd02c2b9c6c7b6c90bc93dd9994b8
snpss10 <sinpossio85@gmail.com>
2023-10-26 09:17:28 -0500

    payphone v1.6

--- Payments-SSD/payphonenots.php
+++ Payments-SSD/payphonenots.php
@@ -1,12 +1,19 @@
 <?php
-
 include '../env.php';
 include '../db.php';
-include ROOT_PATH.'/sys/helpers.php';
+include '../sys/helpers.php';
+include '../payphone/sys/k.php';
+include '../payphone/sys/helpers.php';
+//include '../payphone/api/payphonecontroller.php';
+
+/*
+include '../env.php';
+include '../db.php';
+include '../sys/helpers.php';
 include ROOT_PATH.'/payphone/sys/k.php';
 include ROOT_PATH.'/payphone/sys/helpers.php';
 include ROOT_PATH.'/payphone/api/payphonecontroller.php';
-
+*/
 
 //Obtener parametros de la URL enviados por PayPhone
 $transaccion = $_GET["id"];
@@ -17,9 +24,47 @@ $data_array = array(
     "clientTxId" => $client
 );
 
+echo "id: ".$transaccion;
+echo "clientTxId: ".$client;
 //$rq = [];
 //$rq['id']=(int)$transaccion;
 //$rq['clientTxId']=$client;
+
+
+
+
+//Preparar JSON de llamada
+/*
+$data_array = array(
+    "id"=> (int)$transaccion,
+    "clientTxId"=>$client );
+    
+    $data = json_encode($data_array);
+    
+    //Iniciar Llamada
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://pay.payphonetodoesposible.com/api/button/V2/Confirm");
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt_array($curl, array(
+    CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer TU TOKEN DE AUTENTICACIÓN", "Content-Type:application/json"),
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 $create_response_apibuttonV2confirm = api_button_V2_Confirm ($data_array);
