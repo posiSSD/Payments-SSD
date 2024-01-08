9fff5111595b9049413bf51e104e5605860d83b7
snpss10 <sinpossio85@gmail.com>
2024-01-08 17:10:28 -0500

    payment id v1.498

--- post.php
+++ post.php
@@ -16,7 +16,8 @@ $bc_param["resource"] = "TerminalCallbackPG";
 $bc_param["secretkey"] = '3e_lfs3syayUBEpyx1FD09A4K66scfjmDLvBBuirB0iGsNvndfcaAxbX3O0bSfoXl86aH87G6hKQ2nMJhB9dP7k1tqAnA5LDymAmBmE0fgQr8dwr7DNXa_vVN6LJH1US4i7yxia08TA_wUPYSPwn3mecajkX5abz6w-k9-Yo5SAnBlP6AInSOSo_maCuv88q_G68JjLhEJKhBrp_7aeVdgwLalLbGfY81NbIepdTEMOkP_iNjHaJNT2bQABfktMzZ007Orin5CqaD3CVJcJpe9SAucxQswwrTGIEenH11mKHDX15jWe5tH_GEl0M4yga6X9JAQ';
 $bc_param["sid"] = "279";
 $bc_param["currency"] = "USD";
-$bs_param["paymentID"] = 366;
+//$bs_param["paymentID"] = 366;
+$bs_param["paymentID"] = 2064;
 
 $url_data["currency"] = $bc_param["currency"];
 $url_data["sid"] = $bc_param["sid"];
