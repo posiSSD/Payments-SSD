5d778a586b46a8cee99f614de6bcef903660573d
snpss10 <sinpossio85@gmail.com>
2024-01-25 14:44:41 -0500

    Payment.app v2.2401

--- Payments-SSD/api/payphonePayment.php
+++ Payments-SSD/api/payphonePayment.php
@@ -50,7 +50,7 @@ function payment_curl($url_data){
 	$bc_param["secretkey"]=env('BC_PAYPHONE_SECRET_KEY');
 	$bc_param["sid"]="18751709";
 	$bc_param["currency"]="USD";
-	$bs_param["paymentID"]="366"; //payphone  - 366 //FUNCIONA  - 51
+	$bs_param["paymentID"]="51"; //payphone  - 366 //FUNCIONA  - 51
 
     $url_data["currency"]=$bc_param["currency"];
 	$url_data["sid"]=$bc_param["sid"];
