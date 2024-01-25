b48facec2fe3b4ff56fb02fe50fdf2a2beadce28
snpss10 <sinpossio85@gmail.com>
2024-01-25 11:13:57 -0500

    Payment.app v2.2385

--- cajitapagos2.php
+++ cajitapagos2.php
@@ -1,3 +1,9 @@
+<?php
+require 'env.php'; //desde la raiz a otro lado
+require 'db.php';   
+include ROOT_PATH.'/sys/helpers.php';
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
--- index.php
+++ index.php
@@ -150,119 +150,40 @@ if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], "totalbe
         <?php
     }
 }else{
-    ////////Desde https://payments.totalbet.com/ - cambiar $auth_token, $user_id, $metodo
-    $visit = [];
-    $visit["init"]=date("Y-m-d H:i:s");
-    $visit['ip']=$_SERVER['REMOTE_ADDR'];
-    $visit['device']=$_SERVER['HTTP_USER_AGENT'];
-    $visit['ref']=(array_key_exists('HTTP_REFERER', $_SERVER)?$_SERVER['HTTP_REFERER']:'direct');
-    $visit['url']=$_SERVER['REQUEST_URI'];
+?>   
 
-    // Manejo de los datos enviados por URI
-    $auth_data = null;
-    if(isset($_GET['auth_data'])){
-        $auth_data = json_decode($_GET["auth_data"],true);
-        $auth_token = $auth_data["auth_token"]; 
-        $user_id = $auth_data["user_id"];
-        $metodo = $auth_data["metodo"];
-        $visit["auth_data"]=$auth_data;
-    }else{
-        $auth_token = '0F042972D06A43D60C49525695F5EAB3';
-        $user_id = '1674627753';
-        $metodo = "payphone";
-        $auth_data = array("auth_token" => $auth_token, "user_id" => $user_id, "metodo" => $metodo);
-        $visit["auth_data"]=$auth_data;
-    }
-
-    // conustruccion de URL 
-    $fv=time();
-    $url = 'http';
-    $url.= (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 's':'');
-    $url.= '://';
-    $url.= (isset($_SERVER["HTTP_HOST"]) ? substr($_SERVER['HTTP_HOST'],0):"");
-    $url.= '/';
-    $url.= $metodo;
-    $url.= '/';
-
-    // save logs
-    $log_dir = str_replace(strrchr($_SERVER['SCRIPT_FILENAME'], "/"), "", $_SERVER['SCRIPT_FILENAME'])."/log/";
-    $log_file = date("Y-m-d").".log";
-    log_init($log_dir,$log_file);
-    log_write($visit);
-     
-    // registro en bd de la visita.  
-    $visit_insert = "INSERT INTO 
-					tbl_visits 
-					(url,ip,server_info) 
-					VALUES ('".$visit["url"]."','".$visit['ip']."','".print_r($_SERVER,true)."')";
-    $mysqli->query($visit_insert);
-    if($mysqli->error){
-        echo $mysqli->error; die; 
-    }
-    $mysqli->close();
-
-    if($auth_data){
-        ?>
-        
-        
-        <script type="text/javascript">
-			var this_url = "<?php echo $url;?>";
-			var user_id= <?php echo $user_id;?>;
-			var auth_token="<?php echo $auth_token;?>";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tu Título</title>
+    
+    <script type="text/javascript">
+			var this_url = "<?php echo 'https://payments.totalbet.com/payphone/';?>";
+			var user_id= <?php echo '1674627753';?>;
+			var auth_token="<?php echo '0F042972D06A43D60C49525695F5EAB3';?>";
 		</script>
-        <script type="text/javascript" src="<?php echo $url;?>js/jquery-3.6.0.min.js?<?php echo $fv;?>"></script>	
-        <!-- Añade el script principal payphone -->
-		<script type="text/javascript" src="<?php echo $url;?>js/bc_ws.js?<?php echo $fv;?>"></script>
-		<script type="text/javascript" src="<?php echo $url;?>js/k.js?<?php echo $fv;?>"></script>
-        
+    <script type="text/javascript" src="/payphone/js/jquery-3.6.0.min.js?1706194641"></script>	
+    <script type="text/javascript" src="/payphone/js/bc_ws.js?1706194641"></script>
+    <script type="text/javascript" src="/payphone/js/k.js?1706194641"></script>		
+    <script type="text/javascript" src="cajitas.js?1706194641"></script>	
+    
 
-        <?php
-        $payment_limits=explode(',', env('DEPOSIT_LIMITS'));
-        $payment_limits['min']=number_format($payment_limits[0],0);
-        $payment_limits['max']=number_format($payment_limits[1],0);
-        ?>
-        
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>Payments.app</title>
-            <link rel="stylesheet" href="<?php echo $url; ?>css/k.css">
-            <link rel="stylesheet" href="<?php echo $url; ?>css/new.scss">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-             <!-- Añade el CSS principal payphone -->
-            <link rel="stylesheet" href="https://cdn.payphonetodoesposible.com/box/v1.0/payphone-payment-box.css">           
-            <!-- Añade el script principal payphone -->
-            <script type="module" src="https://cdn.payphonetodoesposible.com/box/v1.0/payphone-payment-box.js"></script>
 
-        </head>
-        <body>
-            <div id="msg" style="font-style: italic;"></div>
-            
-            <div id="pp-button"></div>
-           
-            <div id="kushki_payment_holder">
-                <div id="kushki_details"></div>
-                <br>
-                <div>
-                    <a id="kushki_btn" target="_top">
-                        <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                        <span class="sr-only">Cargando <?php echo $metodo?></span>
-                    </a>
-                </div>
-            </div>
-        </body>
-        </html>
+    <!-- Añade los enlaces CSS -->
+    <link rel="stylesheet" href="https://cdn.payphonetodoesposible.com/box/v1.0/payphone-payment-box.css">
+    
+    <!-- Añade el script principal -->
+    <script type="module" src="https://cdn.payphonetodoesposible.com/box/v1.0/payphone-payment-box.js"></script>
+</head>
+<body>
+    <div id="pp-button"></div>
 
-    <?php
-    }else{
-        ?>
-            <div> <h5 style="text-align: center;  margin-top: 50px;">Por favor vuelva a intentarlo</h5> </div>
-        <?php
-    }
-    ?>  
-  
+   
+</body>
+</html>
+
 <?php
 }
 ?>
