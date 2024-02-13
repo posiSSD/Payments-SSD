1f8a3d58696bea22a29593544ac4c1309808a722
snpss10 <sinpossio85@gmail.com>
2024-02-13 09:12:05 -0500

    Payment.app v2.4 - Springbuilder 1.19

--- index.php
+++ index.php
@@ -173,7 +173,7 @@ if (isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], "totalbe
  }
  
  else{
-     $auth_token = '9B48307736CD353975C31E2DE6BF5CA5';
+     $auth_token = '9261D13C903CFA7AC53560AE172CB88D';
      $user_id = '1674627753';
      $metodo = 'prometeo';
      $amount = 5;
