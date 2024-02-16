431085b5e7d373380fbce3637c3b14bacaf4d54c
snpss10 <sinpossio85@gmail.com>
2024-02-16 15:31:28 -0500

    Payment.app v2.4 - Springbuilder 1.39

--- totalbet.js
+++ totalbet.js
@@ -20,8 +20,8 @@ var observer = new MutationObserver(function(mutationsList, observer) {
                         if (modalContentDiv){
                             
                             // poner margin 0 !important
-                            modalContentDiv.querySelector('.v3-modal.style__ModalWrapper-sc-xfnom2-0.fxHqfr').style.margin = '0';
-                            modalContentDiv.querySelector('.v3-modal.style__ModalWrapper-sc-xfnom2-0.fxHqfr').style.setProperty('margin', '0', 'important');
+                            //modalContentDiv.querySelector('.v3-modal.style__ModalWrapper-sc-xfnom2-0.fxHqfr').style.margin = '0';
+                            //modalContentDiv.querySelector('.v3-modal.style__ModalWrapper-sc-xfnom2-0.fxHqfr').style.setProperty('margin', '0', 'important');
                             // Ocultar el contenido existente del modal
                             modalContentDiv.querySelector('.v3-modal-body').style.display = 'none';
 
