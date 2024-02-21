a095334d5de6b01c487e16a6d6c38e0c38188cf3
snpss10 <sinpossio85@gmail.com>
2024-02-21 16:22:55 -0500

    Payment.app v2.4 - Imagenes-Payphone 1.10

--- logostotalbet.js
+++ logostotalbet.js
@@ -1,42 +1,67 @@
 console.log('Payments Logos loaded.....');
+// Verificar si el body tiene la clase "mobile"
+if (document.body.classList.contains("mobile")) {
+    console.log("móvil.");
 
-// Función que se ejecuta cuando se detecta un cambio en el DOM
-function onMutations(mutationsList, observer) {
-    mutationsList.forEach(function(mutation) {
-        // Verificar si se añadió un nuevo nodo al DOM
-        if (mutation.type === 'childList') {
-            mutation.addedNodes.forEach(function(node) {
-                // Verificar si se añadió un nuevo nodo que es el div deseado
-                if (node.classList && node.classList.contains('accountModal') &&
-                    node.classList.contains('accountModal--desktop')) {
-
-                    console.log('Se detectó la aparición del div deseado:', node);
+    // Función que se ejecuta cuando se detecta el cambio en las clases del div deseado
+    function onClassChange(mutationsList, observer) {
+        mutationsList.forEach(function(mutation) {
+            // Verificar si se modificaron los atributos del div deseado
+            if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+                var targetNode = mutation.target;
+                // Verificar si el div deseado tiene las clases requeridas
+                if (targetNode.classList.contains('style__Container-sc-g7ftgu-1') && 
+                    targetNode.classList.contains('kYpkWH') && 
+                    targetNode.classList.contains('account-popup') && 
+                    targetNode.classList.contains('account-popup-open')) {
+                    console.log('Se detectó el cambio de clases en el div deseado:', targetNode);
                     // Agrega aquí tu código para cargar el script o realizar otras acciones necesarias.
+                    // Obtener el elemento div deseado
+                    var targetNode = document.querySelector('.style__Container-sc-g7ftgu-1.account-popup');
                 }
-            });
-        } 
-        // Verificar si se modificaron los atributos de algún nodo en el DOM
-        else if (mutation.type === 'attributes') {
-            var targetNode = mutation.target;
-            // Verificar si el nodo modificado es el div deseado
-            if (targetNode.classList && 
-                targetNode.classList.contains('style__Container-sc-g7ftgu-1') && 
-                targetNode.classList.contains('kYpkWH') && 
-                targetNode.classList.contains('account-popup') && 
-                targetNode.classList.contains('account-popup-open')) {
-
-                console.log('Se detectó el cambio de clases en el div deseado:', targetNode);
-                // Agrega aquí tu código para cargar el script o realizar otras acciones necesarias.
             }
-        }
-    });
+        });
+    }
+
+    // Crear una instancia de MutationObserver
+    var observer = new MutationObserver(onClassChange);
+    // Observar cambios en los atributos del elemento div deseado
+    observer.observe(targetNode, { attributes: true });
+
+
+} 
+// Verificar si el body tiene la clase "desktop"
+else if (document.body.classList.contains("desktop")) {
+    console.log("desktop.");
+
+    // Función que se ejecuta cuando se detecta la aparición del div deseado
+    function onDivAppear(mutationsList, observer) {
+        mutationsList.forEach(function(mutation) {
+            // Verificar si se añadió un nuevo nodo al DOM
+            if (mutation.type === 'childList') {
+                // Verificar si el nuevo nodo es el div deseado
+                mutation.addedNodes.forEach(function(node) {
+                    if (node.classList && node.classList.contains('accountModal') && node.classList.contains('accountModal--desktop')) {
+                        console.log('Se detectó la aparición del div deseado:', node);
+                        // Agrega aquí tu código para cargar el script o realizar otras acciones necesarias.
+                    }
+                });
+            }
+        });
+    }
+
+    // Crear una instancia de MutationObserver
+    var observer = new MutationObserver(onDivAppear);
+    // Observar cambios en el DOM, incluidos los descendientes del body
+    observer.observe(document.body, { childList: true, subtree: true });
+        
+
+} 
+// En caso de que no tenga ninguna de las clases especificadas
+else {
+    console.log("none.");
 }
 
-// Crear una instancia de MutationObserver para observar cambios en el DOM
-var observer = new MutationObserver(onMutations);
-
-// Observar cambios en el DOM, incluidos los descendientes del body
-observer.observe(document.body, { childList: true, subtree: true });
 
 
 
@@ -162,4 +187,12 @@ var observer = new MutationObserver(onClassChange);
 
 // Observar cambios en los atributos del elemento div deseado
 observer.observe(targetNode, { attributes: true });
+*/
+
+/*
+
+<body class="desktop  txlive " style="background:#FFFFFF;">
+
+<body class="mobile txlive" style="background: rgb(255, 255, 255); overflow: visible;">
+
 */
