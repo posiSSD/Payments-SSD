7b893f0fa955af0d2c309cbbcdc290d41d544521
snpss10 <sinpossio85@gmail.com>
2024-02-21 09:49:08 -0500

    Payment.app v2.4 - Springbuilder 1.71

--- Payments-SSD/api/payphonePayment.php
+++ Payments-SSD/api/payphonePayment.php
@@ -53,7 +53,7 @@ function payment_curl($url_data){
 	$bc_param["sid"]="18751709";
 	$bc_param["currency"]="USD";
     //payphone  - 366 -- 15134 //FUNCIONA  - 51 // 15134 prometeo  //NewPrometeo (14207) //NewPayphone (14177)
-	$bs_param["paymentID"]="15134";
+	$bs_param["paymentID"]="14207";
 
     $url_data["currency"]=$bc_param["currency"];
 	$url_data["sid"]=$bc_param["sid"];
