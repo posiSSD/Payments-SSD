bad2e0a735343f702e97dc5a5531599d673becaa
snpss10 <sinpossio85@gmail.com>
2024-03-01 11:56:23 -0500

    Payment.app v2.84

--- totalbet.js
+++ totalbet.js
@@ -48,12 +48,47 @@ if (document.body.classList.contains("mobile")) {
 
                             var modalElement = modal_root.find('.v3-modal');
                             if(modalElement){
-                                //modalElement.css('margin','0');
-                                //modalElement.css('width','460px');
-                                console.log('modalElement v3-modal FOUND');
+                                modalElement.css('margin','0');
+                                modalElement.css('width','460px');
+                                console.log('v3-modal FOUND');
+
+                                var modalContentDiv = modalElement.find('.v3-modal-content')
+                                if (modalContentDiv) {
+                                    modalContentDiv.css('border-radius','0px');
+                                    console.log('v3-modal-content v3-modal FOUND');
+
+                                    var modal_body = modalContentDiv.find('.v3-modal-body');
+                                    if ( modal_body ) {
+                                        modal_body.css('display','none');
+                                        console.log('v3-modal-body FOUND');
+
+                                    } else {
+                                        console.log('v3-modal-body Not Found');
+                                    }
+                                } else {
+                                    console.log('v3-modal-content v3-modal NOT FOUND');
+                                }
                             }else{
-                                console.log('modalElement v3-modal NOT FOUND');
+                                console.log('v3-modal Not Found');
                             }
+
+                            /*
+                            var modalContentDiv = modal_root.find('.v3-modal-content')
+                            if (modalContentDiv) {
+                                modalContentDiv.css('border-radius','0px');
+                                console.log('v3-modal-content v3-modal FOUND');
+                            } else {
+                                console.log('v3-modal-content v3-modal NOT FOUND');
+                            }
+
+                            var modal_body = modal_root.find('.v3-modal-body');
+                            if ( modal_body ) {
+                                modal_body.css('display','none');
+                                console.log('v3-modal-body FOUND');
+                            } else {
+                                console.log('v3-modal-body Not Found');
+                            }
+                            */
 
                             var authData = localStorage.getItem("x__ACCOUNT__auth_data");
                             if(authData){
@@ -72,7 +107,7 @@ if (document.body.classList.contains("mobile")) {
                             if (carruselActivo.length > 0) {
                                 var opcionSeleccionada = carruselActivo.find('.payment__item-box-text').text().trim().toLowerCase();
                                 var metodo_tb = opcionSeleccionada;
-                                console.log('payment__item-box-active Found');
+                                console.log('payment__item-box-active Found '.metodo_tb);
                             } else {
                                 console.log('payment__item-box-active Not Found');
                             }
