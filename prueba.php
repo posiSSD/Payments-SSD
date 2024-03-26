ab63e818dab16a7957d5d44b4c58c439e77b688a
snpss10 <sinpossio85@gmail.com>
2024-03-26 09:32:30 -0500

    Payment.app v2.56

--- prueba.php
+++ prueba.php
@@ -17,7 +17,7 @@ include 'db.php';
     $bc_param = [];
 	$bc_param["host"]="https://payments1.betconstruct.com/";
 	$bc_param["resource"]="TerminalCallbackPG";
-	$bc_param["secretkey"]=env('BC_PAYPHONE_SECRET_KEY');
+	$bc_param["secretkey"]='wwaw4TbqSrO24gH22';
 	$bc_param["sid"]="18751709";
 	$bc_param["currency"]="USD";
     //payphone  - 366 -- 15134 //FUNCIONA  - 51 // 15134 prometeo  //NewPrometeo (14207) //NewPayphone (14177) // 3803
