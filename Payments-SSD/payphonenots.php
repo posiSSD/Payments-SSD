1406a8647daf147922e0e05c9246d1500afd2e71
snpss10 <sinpossio85@gmail.com>
2024-03-26 13:52:13 -0500

    Payment.app v2.64

--- Payments-SSD/payphonenots.php
+++ Payments-SSD/payphonenots.php
@@ -26,121 +26,121 @@ log_write('_SERVER');
 log_write($_SERVER);
 log_write('json');
 
-$a=[];
-$ret=[];
-$http_code = 500;
-$status = 'Error';
-$response = [];
-//$limit_try = 0;
+try {
+    
+    //$payphone_array_response = dataconstruccion ($data_array);
+    //comprobacion si la tranx existe para evitar duplicados 
+    $status_payphone_transactions = payphone_status_transaction($data_array); 
+    if (!$status_payphone_transactions){
 
-//$payphone_array_response = dataconstruccion ($data_array);
+        // obtener detalles de la tx en la api de payphone
+        $payphone_array_response = payphone_api_confirm ($data_array);
 
-//comprobacion si la tranx existe para evitar duplicados 
-$status_payphone_transactions = payphone_status_transaction($data_array); 
-if (!$status_payphone_transactions){
+        if($payphone_array_response){
 
-    // obtener detalles de la tx en la api de payphone
-    $payphone_array_response = payphone_api_confirm ($data_array);
+            // guardar en la BD
+            payphone_api_transactions($payphone_array_response);
 
-    if($payphone_array_response){
+            // obtener client_id, unique_id, amount,  
+            $data_array_response_details = payphone_bd_details($payphone_array_response);
 
-        // guardar en la BD
-        payphone_api_transactions($payphone_array_response);
+            $payphone_array_response['client_id'] = $data_array_response_details['client_id'];
+            $payphone_array_response['Response'] = "True";
+            $payphone_array_response['Time'] = (new DateTime('now', new DateTimeZone('America/Lima')))->format('Y-m-d H:i:s');
+            log_write($payphone_array_response);
 
-        // obtener client_id, unique_id, amount,  
-        $data_array_response_details = payphone_bd_details($payphone_array_response);
+            switch ($payphone_array_response['transactionStatus']){
 
-        $payphone_array_response['client_id'] = $data_array_response_details['client_id'];
-        $payphone_array_response['Response'] = "True";
-        $payphone_array_response['Time'] = (new DateTime('now', new DateTimeZone('America/Lima')))->format('Y-m-d H:i:s');
-        log_write($payphone_array_response);
+                case "Approved":
+                    
+                    $new_trans=[];
+                    $new_trans['unique_id']=$data_array_response_details['unique_id'];
+                    $new_trans['client_id']=$data_array_response_details['client_id'];
+                    $new_trans['status']=9; // 3=pending deposit
+                    $new_trans['order_id']=$payphone_array_response['transactionId'];
+                    $new_trans['payment_id']=$payphone_array_response['transactionId'];
+                    create_or_update_transaction($new_trans);
+                    sleep(5);
+                    // llamar BC
+                    $d=[];
+                    $d['account']=$data_array_response_details['client_id'];
+                    $d['amount']=$data_array_response_details['amount'];
+                    $d['order_id']=$payphone_array_response['transactionId'];
+                    $d['payment_method']='payphone'; // 4 = payphone
 
-        switch ($payphone_array_response['transactionStatus']){
-
-            case "Approved":
-                
-                $new_trans=[];
-                $new_trans['unique_id']=$data_array_response_details['unique_id'];
-                $new_trans['client_id']=$data_array_response_details['client_id'];
-                $new_trans['status']=9; // 3=pending deposit
-                $new_trans['order_id']=$payphone_array_response['transactionId'];
-                $new_trans['payment_id']=$payphone_array_response['transactionId'];
-                create_or_update_transaction($new_trans);
-                sleep(5);
-                // llamar BC
-                $d=[];
-                $d['account']=$data_array_response_details['client_id'];
-                $d['amount']=$data_array_response_details['amount'];
-                $d['order_id']=$payphone_array_response['transactionId'];
-                $d['payment_method']='payphone'; // 4 = payphone
-
-                $bc_deposit = bc_deposit($d);
-                //consolelogdata($bc_deposit);
-                if(array_key_exists('http_code', $bc_deposit)){
-                    if ($bc_deposit['http_code']==200){
-                        $new_trans=[];
-                        $new_trans['unique_id']=$data_array_response_details['unique_id'];
-                        $new_trans['client_id']=$data_array_response_details['client_id'];
-                        $new_trans['status']=7; // 3=paid
-                        $new_trans['wallet_id']=$bc_deposit['result']['trx_id'];
-                        $new_trans['payment_id']=$payphone_array_response['transactionId'];
-                        create_or_update_transaction($new_trans);
-                        $ret['http_code'] = 200;
-                        $ret['status'] = 'Ok';
-                        $ret['response'] = 'Order '.$id.' paid';
-                        // $id = order_id
-                        api_ret($ret);
-                    } else {
-                        $new_trans=[];
-                        $new_trans['unique_id']=$data_array_response_details['unique_id'];
-                        $new_trans['client_id']=$data_array_response_details['client_id'];
-                        $new_trans['status']=11; // 11 = 5 = failed deposit
-                        $new_trans['payment_id']=$payphone_array_response['transactionId'];
-                        create_or_update_transaction($new_trans);
-                        $ret['http_code']=$bc_deposit['http_code'];
-                        $ret['status']='Error';
-                        $ret['response']='Order '.$id.' wrong / check logs';
-                        // $id = order_id
-                        //$ret['try'] = 'Try :'.$limit_try;
-                        api_ret($ret);
+                    $bc_deposit = bc_deposit($d);
+                    //consolelogdata($bc_deposit);
+                    if(array_key_exists('http_code', $bc_deposit)){
+                        if ($bc_deposit['http_code']==200){
+                            $new_trans=[];
+                            $new_trans['unique_id']=$data_array_response_details['unique_id'];
+                            $new_trans['client_id']=$data_array_response_details['client_id'];
+                            $new_trans['status']=7; // 3=paid
+                            $new_trans['wallet_id']=$bc_deposit['result']['trx_id'];
+                            $new_trans['payment_id']=$payphone_array_response['transactionId'];
+                            create_or_update_transaction($new_trans);
+                            $ret['http_code'] = 200;
+                            $ret['status'] = 'Ok';
+                            $ret['response'] = 'Order '.$id.' paid';
+                            // $id = order_id
+                            api_ret($ret);
+                        } else {
+                            $new_trans=[];
+                            $new_trans['unique_id']=$data_array_response_details['unique_id'];
+                            $new_trans['client_id']=$data_array_response_details['client_id'];
+                            $new_trans['status']=11; // 11 = 5 = failed deposit
+                            $new_trans['payment_id']=$payphone_array_response['transactionId'];
+                            create_or_update_transaction($new_trans);
+                            $ret['http_code']=$bc_deposit['http_code'];
+                            $ret['status']='Error';
+                            $ret['response']='Order '.$id.' wrong / check logs';
+                            // $id = order_id
+                            //$ret['try'] = 'Try :'.$limit_try;
+                            api_ret($ret);
+                        }
                     }
-                }
- 
+    
 
-                exit();
+                    exit();
 
-            break;
+                break;
 
-            case "Canceled": 
-                $new_trans=[];
-                $new_trans['unique_id']=$data_array_response_details['unique_id'];
-                $new_trans['client_id']=$data_array_response_details['client_id'];
-                $new_trans['status']=10; // 10 = 4=declined by payment
-                create_or_update_transaction($new_trans);
-                $ret['http_code']=400;
-                $ret['status']='Canceled';
-                $ret['response']='Order '.$id.' Canceled';
-                // $id = order_id
-                api_ret($ret);
-                exit();   
-            break;    
-        }
+                case "Canceled": 
+                    $new_trans=[];
+                    $new_trans['unique_id']=$data_array_response_details['unique_id'];
+                    $new_trans['client_id']=$data_array_response_details['client_id'];
+                    $new_trans['status']=10; // 10 = 4=declined by payment
+                    create_or_update_transaction($new_trans);
+                    $ret['http_code']=400;
+                    $ret['status']='Canceled';
+                    $ret['response']='Order '.$id.' Canceled';
+                    // $id = order_id
+                    api_ret($ret);
+                    exit();   
+                break;    
+            }
+        } else {
+            
+            $data_array['Response'] = "Datos Nulos";
+            $data_array['Time'] = (new DateTime('now', new DateTimeZone('America/Lima')))->format('Y-m-d H:i:s');
+            log_write('json');
+            log_write($data_array);
+            exit();
+
+        }  
     } else {
-        
-        $data_array['Response'] = "Datos Nulos";
+        $data_array['Response'] = "Duplicity";
         $data_array['Time'] = (new DateTime('now', new DateTimeZone('America/Lima')))->format('Y-m-d H:i:s');
         log_write('json');
         log_write($data_array);
         exit();
 
-    }  
-} else {
-    $data_array['Response'] = "Duplicity";
-    $data_array['Time'] = (new DateTime('now', new DateTimeZone('America/Lima')))->format('Y-m-d H:i:s');
-    log_write('json');
-    log_write($data_array);
-    exit();
-
+    }
+    
+} catch (Exception $e) {
+    // Captura cualquier excepción y muestra el mensaje de error en la consola del navegador
+    $error_message = $e->getMessage();
+    echo "<script>console.error('Error: $error_message');</script>";
 }
 
 function api_ret($r){
