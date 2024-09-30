a861e0dfab39882779f0a58a60f6fb1aa5d7aff8
snpss10 <sinpossio85@gmail.com>
2024-09-30 11:43:36 -0500

    payments 1.62

--- Payments-SSD/payphonenots.php
+++ Payments-SSD/payphonenots.php
@@ -31,10 +31,12 @@ try {
     //$payphone_array_response = dataconstruccion ($data_array);
     //comprobacion si la tranx existe para evitar duplicados 
     $status_payphone_transactions = payphone_status_transaction($data_array); 
+    var_dump($status_payphone_transactions);
     if (!$status_payphone_transactions){
 
         // obtener detalles de la tx en la api de payphone
         $payphone_array_response = payphone_api_confirm ($data_array);
+        var_dump($payphone_array_response);
 
         if($payphone_array_response){
 
@@ -43,6 +45,7 @@ try {
 
             // obtener client_id, unique_id, amount,  
             $data_array_response_details = payphone_bd_details($payphone_array_response);
+            var_dump($data_array_response_details);
 
             $payphone_array_response['client_id'] = $data_array_response_details['client_id'];
             $payphone_array_response['Response'] = "True";
